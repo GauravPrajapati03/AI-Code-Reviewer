7a47641514511cbe41e34d8d753e3b893bd3a471
Gaurav Prajapati <gauravprajapati9370@gmail.com>
2025-03-06 23:49:06 +0530

    Server Created in Backend

---
+++ Backend/src/app.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+
+
+app.use(express.json())
+app.get('/', (req, res) => {
+    res.send(`<h1> Hello </h1>`);
+})
+
+
+
+module.exports = app;
