bc02cd37181e4cbdafbd40ad609777465112720d
Gaurav Prajapati <gauravprajapati9370@gmail.com>
2025-06-20 11:14:12 +0530

    Updated title and vercel.json

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,9 +5,10 @@ const aiRoutes = require('./routes/ai.routes')
 
 
 app.use(cors())
-app.use(express.json())
+app.use(express.json());
+
 app.get('/', (req, res) => {
-    res.send(`<h1> Hello </h1>`);
+    res.send(`Server Running`);
 })
 
 app.use('/ai', aiRoutes);
